38666358f897dc6f696099d37070cd28b912234e
dguis <guistiboys4@gmail.com>
2017-04-05 09:44:07 -0700

    Update JavaScriptSG.js

--- JavaScriptSG.js
+++ JavaScriptSG.js
@@ -1,1 +1,79 @@
+ var coming = function(){
 
+
+
+    alert("Coming Soon!");}
+
+
+
+  var scratch = function(){
+
+
+
+  var sg = document.getElementById("scratch");
+
+
+
+    sg.style.display = "";
+
+
+
+  }
+
+
+
+  var birthday = function(){
+
+
+
+      var bg = document.getElementById("birthday");
+
+
+
+      bg.style.display = "";
+
+
+
+  }
+
+  var luke = function(){
+
+
+
+      var lg = document.getElementById("luke");
+
+
+
+      lg.style.display = "";
+
+
+
+  }
+
+var csg = function(){
+
+    var sg = document.getElementById("scratch");
+
+    sg.style.display = "none";
+
+}
+
+var csbg = function(){
+
+    var bg = document.getElementById("birthday");
+
+
+
+   bg.style.display = "none";   
+
+}
+
+var cslg = function(){
+
+    var lg = document.getElementById("luke");
+
+
+
+   lg.style.display = "none";   
+
+}
